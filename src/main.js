0ed5f33a39b7a456770e6467545655003ea738f9
EthanOwensGit <owensethan02@gmail.com>
2024-08-22 09:54:36 +1000

    Completed P/C tasks

--- src/main.js
+++ src/main.js
@@ -4,17 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
-import DataTable from 'primevue/datatable'
-import Column from 'primevue/column'
 
 const app = createApp(App)
 app.use(PrimeVue, { theme: { preset: Aura } })
-
-app.component('DataTable', DataTable)
-// eslint-disable-next-line vue/multi-word-component-names
-app.component('Column', Column)
+app.use(router)
 
 app.mount('#app')
