aa8b43db59fb44d66c4488835eececc21af4829b
EthanOwensGit <owensethan02@gmail.com>
2024-10-14 14:46:18 +1100

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.MODE === 'production' ? '/eowens-library/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/eowens-library/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
